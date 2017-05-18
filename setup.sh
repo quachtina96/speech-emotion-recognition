e3ef8847f424190513e79d637542b2513a841fb2
AmandaKe <keam@mit.edu>
2017-05-17 23:27:59 -0400

    modified steup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,8 @@
 unset SSH_ASKPASS
 
 echo "adding python modules"
-module load engaging/python/3.5.1
+module load engaging/python/3.5.0
+pip3 install --user h5py
 pip3 install --user pandas
 pip3 install --user keras
 pip3 install --user numpy
@@ -12,5 +13,6 @@ pip3 install --user pydot
 pip3 install --user graphviz
 pip3 install --user tensorflow-gpu
 pip3 install --user theano
+pip3 install --user --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0-cp35-cp35m-linux_x86_64.whl
 #apt-get install graphviz
 echo "done setting up python"
